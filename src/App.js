b0500c749466f02a09eef119ffadbc74a3828024
Sneha-c-c <snehacc2002@gmail.com>
2024-03-06 19:56:47 +0530

    did hello

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import Menu from './Menu';
 import './styles.css';
+import filmIcon from './assets/film-icon.jpg';
 
 const options = [
   { id: 1, label: 'Default option', icon: null },
   { id: 2, label: 'Hovered option', icon: null },
   { id: 3, label: 'Disabled option', icon: null, disabled: true} ,
   { id: 4, label: 'Text option ', icon: null },
-  { id: 4, label: 'Icon and Text Option', icon: 'assets/film-icon.jpg' },
+  { id: 4, label: 'Icon and Text Option', icon: filmIcon },
 ];
 
 function App() {
